ccf1d04105127b52e6871abda368dd3dc9890c28
BlocDeDirt <94722450+BlocDeDirt@users.noreply.github.com>
2023-04-18 09:52:30 +0200

    -

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Tracking from './education/tracking/Tracking'
+
+function App() {
+  return (
+    <div className="App">
+      <Tracking></Tracking>
+    </div>
+  )
+}
+
+export default App
